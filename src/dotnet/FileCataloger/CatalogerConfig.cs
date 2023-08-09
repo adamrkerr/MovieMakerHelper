304360ffade62e532469df8d44ff4f52d7b290df
Adam Kerr <adamrkerr@gmail.com>
2023-08-09 14:51:58 -0500

    catalogger loads from config

---
+++ src/dotnet/FileCataloger/CatalogerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCataloger
+{
+    internal class CatalogerConfig
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public String SearchDirectory { get; set; }
+        public String OutputDirectoryFormat { get; set; }
+        public String[] MovieExtensions { get; set; }
+        public String[] IgnoreNames { get; set; }
+    }
+}
--- src/dotnet/FileCataloger/Program.cs
+++ src/dotnet/FileCataloger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FileSystemCrawler;
 using Microsoft.DirectX.AudioVideoPlayback;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,31 +12,55 @@ namespace FileCataloger
 {
     class Program
     {
-        private static DateTime _startDate = new DateTime(2013, 1, 1);
-        private static DateTime _endDate = new DateTime(2014, 1, 1);
-        private const string _searchDirectory = "F:\\";
-        private const string _outputDirectoryFormat = "C:\\Users\\Adam\\Videos\\Reports\\Old Drive {0}.csv";
-        static readonly string[] _movieExtensions = { ".mp4", ".mov", ".mts", ".avi", ".mpg", ".mpeg", ".asf", ".3gp" };
-        static readonly string[] _ignoreNames = { "itunes", "top gear", "valve", "xgames", "top.gear", "pocket_lint", "fireproof",
-            "\\zip disks\\", "\\videos\\", "\\local disk (g)\\movies\\", "\\my movies\\", "\\my documents\\my videos\\",
-            "video0054.mp4", "video0056.mp4", "video0058.mp4", "video0061.mp4", "video0062.mp4", "video0063.mp4", "video0068.mp4",
-            "video0072.mp4", "video0070.mp4", "video0071.mp4" };
 
         [STAThread]
         static void Main(string[] args)
         {
-            var uniqueFileCrawler = new UniqueFileCrawler(new WindowsCrawlerAssistant(), _ignoreNames, _movieExtensions);
+            var configuration = LoadConfiguration();
 
-            var files = uniqueFileCrawler.CrawlFileSystem(_searchDirectory, _startDate, _endDate);
+            var uniqueFileCrawler = new UniqueFileCrawler(new WindowsCrawlerAssistant(), configuration.IgnoreNames, configuration.MovieExtensions);
+
+            var files = uniqueFileCrawler.CrawlFileSystem(configuration.SearchDirectory, configuration.StartDate, configuration.EndDate);
 
             var reportString = GenerateFileReport(files);
-            var reportFileName = string.Format(_outputDirectoryFormat, $"{_startDate:yyyyMMdd} to {_endDate:yyyyMMdd}");
+            var reportFileName = string.Format(configuration.OutputDirectoryFormat, $"{configuration.StartDate:yyyyMMdd} to {configuration.EndDate:yyyyMMdd}");
             using(var writer = new StreamWriter(reportFileName))
             {
                 writer.Write(reportString);
             }
 
             Console.ReadKey();
+        }
+
+        private static CatalogerConfig LoadConfiguration()
+        {
+            var availableConfigs = Directory.GetFiles("Configurations", "*.json", SearchOption.AllDirectories);
+
+            Console.WriteLine("Please select a configuration:");
+
+            var counter = 1;
+            foreach (var availableConfig in availableConfigs)
+            {
+                Console.WriteLine($"{counter}: {availableConfigs[counter - 1]}");
+                counter++;
+            }
+
+            var selection = 0;
+            while (selection < 1 || selection > availableConfigs.Length)
+            {
+                var s = Console.ReadLine();
+
+                int.TryParse(s, out selection);
+            }
+
+            var content = File.ReadAllText(availableConfigs[selection - 1]);
+            JsonSerializerSettings formatSettings = new JsonSerializerSettings
+            {
+                DateFormatString = "yyyyMMdd"
+            };
+
+
+            return JsonConvert.DeserializeObject<CatalogerConfig>(content, formatSettings);
         }
 
 
@@ -68,6 +93,7 @@ namespace FileCataloger
             try
             {
                 using (
+                    
                     var video = new Video(file.FileInfo.FullName, false))
                 {
                     file.Duration = video.Duration;
